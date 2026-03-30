f67454bf741701fe7e9fea50f4aa3fe4e683cf63
Bryce Klinker <bryce.klinker@gmail.com>
2026-03-30 13:55:55 -0500

    fix: enforce AZURE_LOCATION env var in composite action and use async validation in ValidationBehaviour

--- src/FootballPlanner.Application/Behaviours/ValidationBehaviour.cs
+++ src/FootballPlanner.Application/Behaviours/ValidationBehaviour.cs
@@ -16,8 +16,9 @@ public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRe
             return await next();
 
         var context = new ValidationContext<TRequest>(request);
-        var failures = validators
-            .Select(v => v.Validate(context))
+        var results = await Task.WhenAll(
+            validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var failures = results
             .SelectMany(r => r.Errors)
             .Where(f => f != null)
             .ToList();

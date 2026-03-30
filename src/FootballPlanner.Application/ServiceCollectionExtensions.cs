3498c5aa14f058a35be27ac7baea12a513ca455f
Bryce Klinker <bryce.klinker@gmail.com>
2026-03-30 07:33:57 -0500

    feat: add CQRS application infrastructure with ValidationBehaviour

---
+++ src/FootballPlanner.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using FootballPlanner.Application.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FootballPlanner.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        });
+        services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
+        return services;
+    }
+}
